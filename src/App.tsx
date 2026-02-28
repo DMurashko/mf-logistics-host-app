31e0bb9ae753967c9c389843023c4cd82b2f0fb8
Dmytro Murashko <appleitunesstore590@gmail.com>
2026-02-28 18:05:06 +0100

    feat: add theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css';
 import { LoginPage } from 'login_app/LoginPage';
+import { ThemeProvider } from 'ui_library/Theme';
 
 function App() {
   return (
-    <>
+    <ThemeProvider>
       <LoginPage />
-    </>
+    </ThemeProvider>
   );
 }
 
--- src/remotes.d.ts
+++ src/remotes.d.ts
@@ -3,6 +3,11 @@ declare module 'login_app/LoginPage' {
   export const LoginPage: React.ComponentType;
 }
 
+declare module 'ui_library/Theme' {
+  import React, { ReactNode } from 'react';
+  export const ThemeProvider: React.ComponentType<{ children: ReactNode }>;
+}
+
 declare module 'ui_library/*' {
   const Component: any;
   export default Component;
